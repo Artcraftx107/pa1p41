8539ef7cc94c02fad8dfd8389b6b75a876c0c006
Artcraft <arturoao@uma.es>
2024-04-23 11:01:06 +0200

    Refactored PruebaAsignatura

--- src/main/java/PruebaAsignatura.java
+++ src/main/java/PruebaAsignatura.java
@@ -1,6 +1,10 @@
 import notas.*;
 public class PruebaAsignatura {
+   private static final String[] als={
+           "12455666F;Lopez Perez, Pedro;6.7",
+           "33678999D;Merlo Gomez, Isabel;5.8",
+           "23555875G;Martinez Herrera, Lucia;9.1"};
     public static void main(String[] args){
-        
+
     }
 }
